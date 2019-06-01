afb77e7c42286f2c870c8cc1cbfc70fb5f3ef91f
aman thakali <amanthakali@hotmail.com>
2019-06-01 13:52:05 -0400

    run built

---
+++ client/build/precache-manifest.cdf670dab493801070b95afeb5884791.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "50324a0302b5c4fb0bec",
+    "url": "/static/css/main.46aafdb6.chunk.css"
+  },
+  {
+    "revision": "50324a0302b5c4fb0bec",
+    "url": "/static/js/main.de97e280.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "08e60256e97de3e5b743",
+    "url": "/static/js/2.8006c6cb.chunk.js"
+  },
+  {
+    "revision": "b4159aadfa200b52f11c3049590b1d74",
+    "url": "/index.html"
+  }
+];
